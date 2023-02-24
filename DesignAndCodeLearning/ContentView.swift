e66f4c453a94ac6fd91423c13fa0139f9a872255
Dark Eternal Code <kchanproseth@gmail.com>
2023-02-23 23:13:15 -0600

    Initial Commit

---
+++ DesignAndCodeLearning/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  DesignAndCodeLearning
+//
+//  Created by Kan Chanproseth on 23/02/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DesignAndCodeLearning/DesignAndCodeLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignAndCodeLearningApp.swift
+//  DesignAndCodeLearning
+//
+//  Created by Kan Chanproseth on 23/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct DesignAndCodeLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
